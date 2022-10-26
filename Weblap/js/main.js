4878b9a707515f2b1e37cff581900de2baa09543
molmardo <molnarmark2005@gmail.com>
2022-10-26 21:46:30 +0200

    angol bevezetőszöveg elkészítve

--- Weblap/js/main.js
+++ Weblap/js/main.js
@@ -1,19 +1,19 @@
-const card_bevezeto_magyar = document.querySelector("card_bevezeto_magyar");
-const card_bevezeto_angol = document.querySelector("card_bevezeto_magyar");
-const hide = document.querySelector("hide");
-const show = document.querySelector("show");
-const einglish_version = document.querySelector("einglish_version");
-const hungary_version = document.querySelector("einglish_version");
-
-
-document.getElementById("para").onclick = function() {  
-    fun()  
-    };  
-function fun(){
-
+function english(){
+    var card_bevezeto_magyar = document.getElementById("card_bevezeto_magyar");
+    var card_bevezeto_angol = document.getElementById("card_bevezeto_angol");
     card_bevezeto_magyar.classList.remove("show");
     card_bevezeto_magyar.classList.add("hide");
     card_bevezeto_angol.classList.remove("hide");
-    card_bevezeto_angol.classList.add("show");
-
+    card_bevezeto_angol.classList.add("show"); 
 }
+
+function hungary(){
+    var card_bevezeto_magyar = document.getElementById("card_bevezeto_magyar");
+    var card_bevezeto_angol = document.getElementById("card_bevezeto_angol");
+    card_bevezeto_magyar.classList.add("show");
+    card_bevezeto_magyar.classList.remove("hide");
+    card_bevezeto_angol.classList.add("hide");
+    card_bevezeto_angol.classList.remove("show"); 
+}
+
+console.log(hide);
